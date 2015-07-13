4e5cdf4bea4fb5d8fb79b9190f7adc30a3a0edf5
KittyeCat <basil.connor@hotmail.co.nz>
2015-07-13 13:31:45 +1200

    Initial commit

---
+++ desktop/src/com/AwesomeGame/4/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.AwesomeGame.4.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.AwesomeGame.4.AwesomeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AwesomeGame(), config);
+	}
+}
