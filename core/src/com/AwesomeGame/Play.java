eeedc358e266b5cf8ec207aca2e2ec4be166a0f3
KittyeCat <basil.connor@hotmail.co.nz>
2015-07-13 14:58:17 +1200

    Have tiled map rendering on screen with a camera

--- core/src/com/AwesomeGame/Play.java
+++ core/src/com/AwesomeGame/Play.java
@@ -1,9 +1,52 @@
 package com.AwesomeGame;
 
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 /**
  * Created by Basil on 13/07/2015.
  */
-public class Play implements Screen {
+public class Play extends ScreenAdapter {
+
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+
+
+    @Override
+    public void show() {
+
+        TmxMapLoader loader = new TmxMapLoader();
+        map = loader.load("core/assets/TestMap.tmx");
+
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        camera = new OrthographicCamera();
+
+
+    }
+    @Override
+    public void dispose() {
+
+        map.dispose();
+        renderer.dispose();
+    }
+
+    @Override
+    public void render(float delta) {
+        renderer.setView(camera);
+        renderer.render();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
+
+    }
 }
